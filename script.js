46b80f5c09a1e3c782d82a94d432a66cee0e8590
Ehmad <justEhmadSaeed@gmail.com>
2020-03-26 22:46:51 +0500

    👌 IMPROVE: ADD JS Clock

---
+++ script.js
@@ -0,0 +1,22 @@
+const secondHand = document.querySelector(".second-hand");
+const minuteHand = document.querySelector(".min-hand");
+const hourHand = document.querySelector(".hour-hand");
+
+const setDate = () => {
+    const now = new Date();
+    // Setting Seconds
+    const sec = now.getSeconds();
+    const secDegree = sec * 6 + 90;
+    secondHand.style.transform = `rotate(${secDegree}deg)`;
+    // Setting minutes
+    const min = now.getMinutes();
+    const minDegree = min * 6 + 90;
+    minuteHand.style.transform = `rotate(${minDegree}deg)`;
+    // Setting hours
+    let hour = now.getHours();
+    // if (hour >= 12) hour -= 12;
+    const hourDegree = hour * 30 + 90;
+    hourHand.style.transform = `rotate(${hourDegree}deg)`;    
+    
+};
+setInterval(setDate, 1000);
